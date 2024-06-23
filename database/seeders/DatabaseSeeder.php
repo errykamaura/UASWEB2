fd74370da06e245830d10899c387a5a41afde10f
Erryka Maura Adysti <errikamaura@gmail.com>
2024-06-23 18:30:31 +0700

    hello

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // \App\Models\User::factory(10)->create();
+
+        \App\Models\User::factory()->create([
+            'name' => 'Admin',
+            'role' => 'Admin',
+            'email' => 'admin123@gmail.com',
+            'password' => Hash::make('admin123')
+        ]);
+    }
+}
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'Admin',
+            'role' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('admin123'),
+        ]);
+        User::create([
+            'name' => 'Customer Coba 01',
+            'role' => 'Customer',
+            'email' => 'cust01@gmail.com',
+            'password' => Hash::make('customer'),
+        ]);
+        User::create([
+            'name' => 'Customer Coba 02',
+            'role' => 'Customer',
+            'email' => 'cust02@gmail.com',
+            'password' => Hash::make('customer'),
+        ]);
+        
+        User::create([
+            'name' => 'Admin',
+            'role' => 'Admin',
+            'email' => 'admin123@gmail.com',
+            'password' => Hash::make('admin123'),
+        ]);
+    }
+}
